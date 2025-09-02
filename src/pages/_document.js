2bfd999e3183a4e3fdc7173a0a18c9e116014c24
AffanShaikh726 <affanshaikh0726@gmail.com>
2025-09-02 22:35:57 +0530

    added favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,60 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="180x180"
+                    href="/apple-icon-180.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/manifest-icon-192.maskable.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="196x196"
+                    href="/favicon-196.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="512x512"
+                    href="/manifest-icon-512.maskable.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-icon-180.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="192x192"
+                    href="/manifest-icon-192.maskable.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="196x196"
+                    href="/favicon-196.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="512x512"
+                    href="/manifest-icon-512.maskable.png"
+                />
+            </Head>
+
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
